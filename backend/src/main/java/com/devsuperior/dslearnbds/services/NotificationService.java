0407bffd4fc14997ea00798617c17ac58abbbda0
rafsousa <rodrigo.alex.sousa@gmail.com>
2021-04-30 19:25:10 -0300

    Observer design pattern, OCP SOLID principle

--- backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -1,24 +1,40 @@
 package com.devsuperior.dslearnbds.services;
 
+import java.time.Instant;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.Deliver;
 import com.devsuperior.dslearnbds.entities.Notification;
 import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.observers.DeliverRevisionObserver;
 import com.devsuperior.dslearnbds.repositories.NotificationRepository;
 
 @Service
-public class NotificationService {
+public class NotificationService implements DeliverRevisionObserver {
 
 	@Autowired
 	private NotificationRepository repository;
 	
 	@Autowired
 	private AuthService authService;
+
+	@Autowired
+	private DeliverService deliverService;
 	
+	@PostConstruct
+	private void initialize() {
+		deliverService.subscribDeliverRevisionObservers(this);
+	}
+	
+	@Transactional(readOnly = true)
 	public Page<NotificationDTO> notificationsForCurrentUser(boolean unreadOnly, Pageable pageable) {
 		User user = authService.authenticated();
 		
@@ -26,4 +42,29 @@ public class NotificationService {
 		
 		return page.map(x -> new NotificationDTO(x));
 	}
+	
+	@Transactional
+	public void saveDeliverNotification(Deliver deliver) {
+		
+		Long sectionId = deliver.getLesson().getSection().getId();
+		Long resourceId = deliver.getLesson().getSection().getResource().getId();
+		Long offerId = deliver.getLesson().getSection().getResource().getOffer().getId();
+
+		String route = "/offers/" + offerId + "/resources/" + resourceId + "/sections/" + sectionId;
+		String text = deliver.getFeedback();
+		Instant moment = Instant.now();
+		
+		User user = deliver.getEnrollment().getStudent();
+		
+		Notification notification = new Notification(null, text, moment, false, route, user);
+		repository.save(notification);
+		
+	}
+
+	@Override
+	public void onSaveRevision(Deliver deliver) {
+		saveDeliverNotification(deliver);
+		
+	}
+	
 }
